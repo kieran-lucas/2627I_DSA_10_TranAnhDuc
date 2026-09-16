2240f78403fe39a78bad93ae31a0b5f39cb20db6
Kieran Lucas <kieranlucas.work@gmail.com>
2026-09-17 06:12:58 +0700

    Implement Exercise 1.4.12 linear common-elements scan

---
+++ Algorithms/week2/Exercises/CommonElements.java
@@ -0,0 +1,39 @@
+import edu.princeton.cs.algs4.*;
+
+public class CommonElements {
+
+    public static void printCommon(int[] a, int[] b) {
+        int i = 0;
+        int j = 0;
+
+        while (i < a.length && j < b.length) {
+
+            if (a[i] < b[j]) {
+                i++;
+            } else if (a[i] > b[j]) {
+                j++;
+            } else {
+                StdOut.print(a[i] + " ");
+                i++;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = StdIn.readInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            b[i] = StdIn.readInt();
+        }
+
+        printCommon(a, b);
+    }
+}
